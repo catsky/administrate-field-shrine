2bc76bd50e86b094f183e97ad8fe215e8ab0ee7f
Ken Zheng <zhdhui@gmail.com>
2017-04-24 09:25:59 +1000

    init commit

---
+++ lib/administrate/field/shrine.rb
@@ -0,0 +1,9 @@
+require "administrate/field/shrine/version"
+
+module Administrate
+  module Field
+    module Shrine
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/administrate/field/shrine/version.rb
@@ -0,0 +1,7 @@
+module Administrate
+  module Field
+    module Shrine
+      VERSION = "0.1.0"
+    end
+  end
+end
