dbee70b479640d7f06af675972e5c79db30e0926
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-06-07 15:09:05 +0200

    Allow attachment version to be specified

--- lib/administrate/field/shrine.rb
+++ lib/administrate/field/shrine.rb
@@ -7,12 +7,29 @@ module Administrate
       class Engine < ::Rails::Engine
       end
 
+      # we should first try the regular data.url because if the versions are generated
+      # in the background by a worker they won't be available immediately after
+      # submitting the form
+      # however, once data[:version].url is available data.url won't work,
+      # so we're covered in both cases
       def url
-        data.try(:url).to_s
+        (regular_url || version_url).to_s
+      end
+
+      def regular_url
+        data.try(:url)
+      end
+
+      def version_url
+        data.try(:[], version).try(:url)
       end
 
       def url_only?
         options.fetch(:url_only, false)
+      end
+
+      def version
+        options.fetch(:version, nil)
       end
 
       def cached_value
