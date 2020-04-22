f289252f572c4c410bfa2a79f4dd2b5d1c10251b
KenZ <zhdhui@gmail.com>
2020-04-23 09:16:45 +1000

    release v0.0.5

--- spec/administrate/field/shrine_spec.rb
+++ spec/administrate/field/shrine_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe Administrate::Field::Shrine do
-  xit "has a version number" do
+  it "has a version number" do
     puts Administrate::Field::Shrine.to_s
   end
 end
